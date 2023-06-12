7072a44b5394a40bb729a1439583b4206bf57ea9
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-11 23:07:12 -0400

    Setup models field for each app

--- backend/diary/models.py
+++ backend/diary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Diary(models.Model):
+    picture = models.ImageField()
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    hair_color = models.CharField(max_length=255)
+    eye_color = models.CharField(max_length=255)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    status_conditions = models.ForeignKey(User, related_name='diary_status_conditions', on_delete=models.CASCADE)
+    life_experience = models.ForeignKey(User, related_name='diary_life_experience', on_delete=models.CASCADE)
+    DNA_profile = models.ForeignKey(User, related_name='diary_DNA', on_delete=models.CASCADE)
+    isAccepted = models.BooleanField()
--- backend/story/models.py
+++ backend/story/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Story(models.Model):
+    place_of_birth = models.CharField(max_length=255)
+    education = models.CharField(max_length=255)
+    number_of_siblings = models.IntegerField()
+    number_of_children = models.IntegerField()
+    number_of_grandchildren = models.IntegerField()
+    biography = models.TextField(max_length=999)
