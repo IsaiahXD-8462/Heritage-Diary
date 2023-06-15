5d65a64a10ef904b6c76923c3e43721f73a88082
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-15 13:24:24 -0400

    Deleted DNA profile serializer

--- backend/diary/serializers.py
+++ backend/diary/serializers.py
@@ -4,9 +4,8 @@ from .models import Diary
 class DiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Diary
-        fields = ['picture', 'first_name', 'middle_name', 'last_name', 'date_of_birth', 'hair_color', 'eye_color', 'height', 'weight', 'status_conditions', 'life_experience', 'DNA_profile']
+        fields = ['picture', 'first_name', 'middle_name', 'last_name', 'date_of_birth', 'hair_color', 'eye_color', 'height', 'weight', 'status_conditions', 'life_experience',]
         depth = 1
 
 status_conditions = serializers.IntegerField(write_only=True)
 life_experience = serializers.IntegerField(write_only=True)
-DNA_profile = serializers.IntegerField(write_only=True)
