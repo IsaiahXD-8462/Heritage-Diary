fce9f74fcd85bc6fa065f90cc1fcfa3036fba5bd
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-16 05:12:29 -0400

    changed url pathways

--- backend/diary/urls.py
+++ backend/diary/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from diary import views
 
 urlpatterns = [
-    path('all/', views.logged_in_user),
+    path('', views.diary_list),
+    path('<int:pk>', views.diary_detail)
 ]
