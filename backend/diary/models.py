101c7c6a11445b89cc272bb4c40aff0f8325764f
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-16 05:12:10 -0400

    Refered to different models

--- backend/diary/models.py
+++ backend/diary/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from authentication.models import User
+from medical.models import Medical
+from story.models import Story
 
 class Diary(models.Model):
     picture = models.ImageField()
@@ -11,6 +13,6 @@ class Diary(models.Model):
     eye_color = models.CharField(max_length=255)
     height = models.IntegerField()
     weight = models.IntegerField()
-    status_conditions = models.ForeignKey(User, related_name='diary_status_conditions', on_delete=models.CASCADE)
-    life_experience = models.ForeignKey(User, related_name='diary_life_experience', on_delete=models.CASCADE)
+    status_conditions = models.ForeignKey(Medical, related_name='diary_status_conditions', on_delete=models.CASCADE)
+    life_experience = models.ForeignKey(Story, related_name='diary_life_experience', on_delete=models.CASCADE)
     isAccepted = models.BooleanField()
