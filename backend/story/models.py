d25340b2c8de8d60f3085c6e741b3a780345f19c
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-20 15:02:10 -0400

    deleted uneccesary fields

--- backend/story/models.py
+++ backend/story/models.py
@@ -3,8 +3,5 @@ from authentication.models import User
 
 class Story(models.Model):
     place_of_birth = models.CharField(max_length=255)
-    education = models.CharField(max_length=255)
-    number_of_siblings = models.IntegerField()
-    number_of_children = models.IntegerField()
-    number_of_grandchildren = models.IntegerField()
+    education = models.TextField(max_length=999)
     biography = models.TextField(max_length=999)
--- backend/story/serializers.py
+++ backend/story/serializers.py
@@ -4,4 +4,4 @@ from .models import Story
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
-        fields = ['place_of_birth', 'education', 'number_of_siblings', 'number_of_children', 'number_of_grandchildren', 'biography']
+        fields = ['place_of_birth', 'education', 'biography']
